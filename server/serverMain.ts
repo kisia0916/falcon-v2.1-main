980c834420338947ae3356685254c7f83dcb4545
fumi <93899734+kisia0916@users.noreply.github.com>
2024-01-23 23:20:34 +0900

    Update serverMain.ts
    change splitSize

--- server/serverMain.ts
+++ server/serverMain.ts
@@ -26,7 +26,7 @@ export interface rastPacketInfoInterface {
 }
 
 export let clientList:clientListInterface[] = []
-export const sendDataSplitSize = 20480
+export const sendDataSplitSize = 2048000
 
 
 
